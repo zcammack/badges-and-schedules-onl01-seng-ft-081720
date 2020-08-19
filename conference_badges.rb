a9b170ac97bdc547ead7f926e71808705eb759ab
Zackary Cammack <zack.cammack@gmail.com>
2020-08-19 23:21:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,13 @@
 def badge_maker(name)
 "Hello, my name is #{name}."
 end
+
+
+def batch_badge_creator(list_of_names)
+  new_list_of_names = []
+  array.each do |name|
+    badge = "Hello, my name is #{name}."
+    new_list_of_names << badge
+  end
+  new_list_of_names
+end
